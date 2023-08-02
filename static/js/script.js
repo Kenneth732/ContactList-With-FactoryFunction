f2af2fe2f6bdc148f9f8f0789aade117fee627a0
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-02 08:17:59 +0300

    createPerson function for creating object of person inforgit

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,14 @@ var personActions = {
         return this.firstName + ' ' + this.lastName + ' ' + this.phoneNumber + ' ' + this.emailAddress
     },
 };
+
+function createPerson(firstName, lastName, phoneNumber, emailAddress) {
+    let person = Object.create(personActions);
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.phoneNumber = phoneNumber;
+    person.emailAddress = emailAddress
+    return person;
+}
+
+
